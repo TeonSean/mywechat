b609feb29385ae06a9041d02b841cfa6b4547a9e
Teon <fmj15@mails.tsinghua.edu.cn>
2017-12-24 18:19:40 +0800

    Defined some actions.

--- mywechat_server/messagedef.h
+++ mywechat_server/messagedef.h
@@ -1,6 +1,16 @@
 #ifndef MESSAGEDEF_H
 #define MESSAGEDEF_H
 
-
+#define ACTION_LOGIN        0
+#define ACTION_LOGOUT       1
+#define ACTION_SEARCH       2
+#define ACTION_ADD          3
+#define ACTION_LIST         4
+#define ACTION_SEND_MSG     5
+#define ACTION_SEND_FILE    6
+#define ACTION_RECV_MSG     7
+#define ACTION_RECV_FILE    8
+#define ACTION_PROFILE      9
+#define ACTION_SYNC         10
 
 #endif // MESSAGEDEF_H
